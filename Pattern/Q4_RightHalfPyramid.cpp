c595b1a35bcf12a9006bb3bae26ad2cd7ef4c735
Farhan-Mr <farhanfarhat0526@gmail.com>
2026-06-27 14:43:52 +0530

    Q4 Right Half Pyramid

---
+++ Pattern/Q4_RightHalfPyramid.cpp
@@ -0,0 +1,21 @@
+// Right Half Pyramid
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int h;
+    cout << "Enter height :- ";
+    cin >> h;
+
+    for ( int i=0; i<h; i++)
+    {
+        for( int j=0; j<=i; j++)
+        {
+            cout << " * ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}

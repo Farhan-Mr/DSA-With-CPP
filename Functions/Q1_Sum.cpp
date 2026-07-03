4237885cbda4522242715e467c90da822f8c0efd
Farhan-Mr <farhanfarhat0526@gmail.com>
2026-07-03 16:20:18 +0530

    Q1 Sum using function

---
+++ Functions/Q1_Sum.cpp
@@ -0,0 +1,16 @@
+/* Sum using function */
+
+#include <iostream>
+
+using namespace std;
+
+void sum(int a, int b)
+{
+    cout << "Sum is " << (a+b);
+}
+
+int main ()
+{
+    sum(10,10);
+    return 0;
+}

fc4965a6bb4cab321b6a1f6796353a4d4b54513d
Farhan-Mr <farhanfarhat0526@gmail.com>
2026-07-09 14:55:34 +0530

    Armstrong number 1 to 1000

--- Functions/Q4.cpp
+++ Functions/Q4.cpp
@@ -1,1 +1,39 @@
-// this is q 4 
+//  Write a function to check if a number is an Armstrong number. 
+// Then, use this function to print all Armstrong numbers between 1 and 1000.
+
+#include <iostream>
+using namespace std;
+
+bool isArmstrong(int num) {
+    int original = num, sum = 0, digits = 0, temp = num;
+
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = num;
+    while (temp != 0) {
+        int digit = temp % 10;
+        int power = 1;
+        for (int i = 0; i < digits; i++) {
+            power *= digit;
+        }
+        sum += power;
+        temp /= 10;
+    }
+
+    return sum == original;
+}
+
+// main function -
+int main() {
+    cout << "Armstrong numbers between 1 and 1000:" << endl;
+    for (int i = 1; i <= 1000; i++) {
+        if (isArmstrong(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
